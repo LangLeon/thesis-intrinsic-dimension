472fd028467464ec019a1c1e4e928631f3c127ca
Leon Lang <leon.lang93@gmail.com>
2019-12-13 21:22:20 +0100

    improve logging and argument parsing

--- classify_mnist.py
+++ classify_mnist.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from mnist import mnist
+import argparse
 
 class Baseline(nn.Module):
     def __init__(self):
@@ -54,30 +55,54 @@ def epoch_iter(model, data, optimizer, loss_function):
 def train_epoch(model, train_loader, val_loader, optimizer, loss_function):
     model.train()
     train_loss, train_acc = epoch_iter(model, train_loader, optimizer, loss_function)
+    print("Train Epoch over. train_loss: {}; train_accuracy: {} \n".format(train_loss, train_acc))
 
     model.eval()
     val_loss, val_acc = epoch_iter(model, val_loader, optimizer, loss_function)
-    print("Epoch over. val_loss: {}; val_accuracy: {} \n".format(val_loss, val_acc))
+    print("Val Epoch over. val_loss: {}; val_accuracy: {} \n".format(val_loss, val_acc))
+    return train_loss, train_acc, val_loss, val_acc
 
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+def main():
 
-n_epochs = 3
-batch_size = 64
-learning_rate = 0.001
-# momentum = 0.5
+    torch.manual_seed(ARGS.seed)
 
-random_seed = 1
-torch.manual_seed(random_seed)
+    train_loader, val_loader, _ = mnist(batch_size = ARGS.batch_size)
+
+    model = Baseline().to(device)
+    optimizer = torch.optim.SGD(model.parameters(), ARGS.lr)
+
+    criterion = nn.CrossEntropyLoss()
 
 
-train_loader, val_loader, _ = mnist(batch_size = batch_size)
+    train_losses = []
+    train_accuracies = []
+    val_losses = []
+    val_accuracies = []
 
-model = Baseline().to(device)
-optimizer = torch.optim.SGD(model.parameters(), learning_rate)
+    for epoch in range(ARGS.n_epochs):
+        print("Epoch {} start".format(epoch+1))
+        train_loss, train_acc, val_loss, val_acc = train_epoch(model, train_loader, val_loader, optimizer, criterion)
+        train_losses.append(train_loss)
+        train_accuracies.append(train_acc)
+        val_losses.append(val_loss)
+        val_accuracies.append(val_acc)
 
-criterion = nn.CrossEntropyLoss()
+    print(train_losses, train_accuracies, val_losses, val_accuracies)
 
-for epoch in range(10):
-    print("Epoch {} start".format(epoch+1))
-    train_epoch(model, train_loader, val_loader, optimizer, criterion)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n_epochs', default=10, type=int,
+                        help='max number of epochs')
+    parser.add_argument('--batch_size', default=64, type=int,
+                        help='max number of epochs')
+    parser.add_argument('--lr', default=0.001, type=float,
+                        help='max number of epochs')
+    parser.add_argument('--seed', default=1, type=int,
+                        help='max number of epochs')
+
+    ARGS = parser.parse_args()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    main()
