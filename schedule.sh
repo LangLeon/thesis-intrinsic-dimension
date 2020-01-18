a204cefa1b2aea9ab3e6aa4e1899ad2ebf1ad9b4
Leon Lang <leon.lang93@gmail.com>
2020-01-18 18:12:47 +0100

    bugfix in job file

--- schedule.sh
+++ schedule.sh
@@ -8,9 +8,12 @@
 source activate thesis
 
 for lr in 0.1 0.01 0.001
+do
 for schedule_gamma in 0.8 0.4 0.2 0.13 0.1
+do
 for schedule_freq in 15 10 5
-do python classify_mnist.py --model=lenet --lr=${lr} --schedule --schedule_gamma=${schedule_gamma} --schedule_freq=${schedule_freq} --n_epochs=50
+do
+python classify_mnist.py --model=lenet --lr=${lr} --schedule --schedule_gamma=${schedule_gamma} --schedule_freq=${schedule_freq} --n_epochs=50
 done
 done
 done
