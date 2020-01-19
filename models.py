30ddce77b12a72e2df0aebd09c3dab4d2818f847
Leon Lang <leon.lang93@gmail.com>
2020-01-19 17:12:24 +0100

    add reg_lenet with conv-restriction in the end

--- models.py
+++ models.py
@@ -267,8 +267,112 @@ class RegularLeNet5LessDownsampling(torch.nn.Module):
         return x
 
 
+class RegularLeNet5LessDownsamplingConvEnd(torch.nn.Module):
+    """
+    Equivariant version of LeNet5.
+
+    Adapted from Lenet:
+    - C8 replaces Id, i.e. the trivial rotation group is replaced by rotations by 45% angles.
+    - Consequently, the regular representation has 8 internal channels. We work with the same number of regular feature
+      spaces in each layer, and thus have, say, 6*8 = 48 effective channels in the second layer (this preserves the number of parameters)
+    - MaxPool2d is replaced by PointwiseMaxPoolAntialiased
+    - relu is replaced by the equivariant relu (also operates pointwise)
+    - We do a group max pooling before the fully connected layer.
+
+    !!!Doc might be wrong!!
+    Input - 1x28x28 triv
+    C1 - 6@24x24 reg (5x5 kernel)
+    relu1
+    S1 - 6@12x12 reg (2x2 kernel, stride 2) Subsampling
+    C2 - 16@8x8 reg (5x5 kernel)
+    relu2
+    S2 - 16@4x4 reg (2x2 kernel, stride 2) Subsampling
+    C3 - 120@1x1 reg (4x4 kernel)
+    relu3
+    S3 - 120@1x1 triv (group pooling)
+    F4 - 84
+    relu4
+    F5 - 10 (Output)
+    """
+    def __init__(self, n_classes=10):
+
+        super(RegularLeNet5LessDownsamplingConvEnd, self).__init__()
+
+        self.r2_act = gspaces.Rot2dOnR2(N=8)
+        in_type = nn2.FieldType(self.r2_act, [self.r2_act.trivial_repr])
+        self.input_type = in_type
+        out_type = nn2.FieldType(self.r2_act, 4*[self.r2_act.regular_repr])
+
+        # block 1
+        self.C11 = nn2.R2Conv(in_type, out_type, kernel_size=3)
+        self.relu11 = nn2.ReLU(out_type)
+        self.C12 = nn2.R2Conv(out_type, out_type, kernel_size=3)
+        self.relu12 = nn2.ReLU(out_type)
+        self.S1 = nn2.PointwiseMaxPoolAntialiased(out_type, kernel_size=2, stride=2)
+
+
+        in_type=self.S1.out_type
+        out_type = nn2.FieldType(self.r2_act, 10*[self.r2_act.regular_repr])
+
+        # block 2
+        self.C21 = nn2.R2Conv(in_type, out_type, kernel_size=3)
+        self.relu21 = nn2.ReLU(out_type)
+        self.C22 = nn2.R2Conv(out_type, out_type, kernel_size=3)
+        self.relu22 = nn2.ReLU(out_type)
+        self.S2 = nn2.PointwiseMaxPoolAntialiased(out_type, kernel_size=2, stride=2)
+
+
+
+        # block 3
+        self.C3 = nn.Conv2d(80, 120, kernel_size=(4, 4))
+        self.relu3 = nn.ReLU()
+
+
+        # block 4: Fully connected
+        self.F4 = nn.Linear(120, 84)
+        self.F5 = nn.Linear(84, 10)
+
+
+        # Complete model
+        self.conv_equi = nn2.SequentialModule(
+            self.C11,
+            self.relu11,
+            self.C12,
+            self.relu12,
+            self.S1,
+            self.C21,
+            self.relu21,
+            self.C22,
+            self.relu22,
+            self.S2
+        )
+
+        self.conv_standard = nn.Sequential(
+            self.C3,
+            self.relu3
+        )
+
+        self.fully = nn.Sequential(
+            self.F4,
+            nn.ReLU(),
+            self.F5
+        )
+
+    def forward(self, input: torch.Tensor):
+
+        x = nn2.GeometricTensor(input, self.input_type)
+        x = self.conv_equi(x)
+        x = x.tensor
+        x = self.conv_standard(x)
+        x = x.squeeze()
+        x = self.fully(x)
+
+        return x
+
+
 models = {
     "MLP": MLP,
     "lenet": LeNet5,
     "reg_lenet": RegularLeNet5,
-    "reg_lenet_2": RegularLeNet5LessDownsampling}
+    "reg_lenet_2": RegularLeNet5LessDownsampling,
+    "reg_lenet_3": RegularLeNet5LessDownsamplingConvEnd}
