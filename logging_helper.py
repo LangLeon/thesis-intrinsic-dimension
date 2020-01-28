0c9d78a59fa7311c18e17a53da65031dfa29b4ab
Leon Lang <leon.lang93@gmail.com>
2020-01-28 22:05:01 +0100

    changed logging_helper

--- logging_helper.py
+++ logging_helper.py
@@ -9,7 +9,7 @@ import numpy as np
 def log_results(epochs, train_losses, train_accuracies, val_losses, val_accuracies, subspace_distances, ARGS):
     rows = zip(epochs, train_losses,train_accuracies,val_losses,val_accuracies, subspace_distances)
 
-    file_name = "d_dim_{}_lr_{}_gamma_{}_sched_freq_{}_seed_{}_epochs_{}_batchsize_{}.csv".format(ARGS.d_dim, ARGS.lr, ARGS.schedule_gamma, ARGS.schedule_freq, ARGS.seed, ARGS.n_epochs, ARGS.batch_size)
+    file_name = "N_{}_flips_{}_d_dim_{}_lr_{}_gamma_{}_sched_freq_{}_seed_{}_epochs_{}_batchsize_{}.csv".format(ARGS.N, ARGS.flips, ARGS.d_dim, ARGS.lr, ARGS.schedule_gamma, ARGS.schedule_freq, ARGS.seed, ARGS.n_epochs, ARGS.batch_size)
 
     subspace_training = "subspace_training" if ARGS.subspace_training else "no_subspace_training"
     model = ARGS.model
