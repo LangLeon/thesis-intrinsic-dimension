2fda57882a35b13ca90dbf7f59b54517def0d13c
Leon Lang <leon.lang93@gmail.com>
2020-01-29 16:23:08 +0100

    added an array job

---
+++ jobs/job_subspace_array.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu_shared
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=Subspace_Array
+#SBATCH --time=08:00:00
+#SBATCH -N 1
+#SBATCH --mem=16000M
+#SBATCH --array=1-16
+#SBATCH --output=slurm_out/table13slim_%A_%a.out
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=leon.lang@student.uva.nl
+source activate thesis
+
+HPARAMS_FILE=$HOME/thesis-intrinsic-dimension/jobs/array_job_hyperparameters.txt
+
+python ddim_vs_acc.py \
+	--model=reg_lenet_3 \
+	--optimizer=SGD \
+	--lr=1 \
+	--schedule \
+	--schedule_gamma=0.3 \
+	--schedule_freq=10 \
+	--seed=1 \
+	--n_epochs=30 \
+	--batch_size=64 \
+	--print_freq=20 \
+	--print_prec=2 \
+	--dense \
+	--parameter_correction \
+ 	$(head -$SLURM_ARRAY_TASK_ID $HPARAMS_FILE | tail -1)
+#	--non_wrapped \
+#	--chunked \
