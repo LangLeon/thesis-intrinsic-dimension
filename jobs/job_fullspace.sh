eb0472cd9739dc8fe2bba7761dfefaca7e58c910
Leon Lang <leon.lang@student.uva.nl>
2020-01-30 14:34:01 +0100

    push results

--- jobs/job_fullspace.sh
+++ jobs/job_fullspace.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-#SBATCH --time=14:00:00
+#SBATCH --time=01:00:00
 #SBATCH -N 1
 #SBATCH --partition=gpu_shared
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16000M
-#SBATCH --output=slurm_out/test_non_wrapped.out
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=leon.lang@student.uva.nl
 source activate thesis
 
 cd $HOME/thesis-intrinsic-dimension
 
-for N in 2 4 6 8 10 12 14 16
-do 
 python classify_mnist.py \
 	--model=table13slim \
-	--N=${N} \
+	--N=1 \
 	--optimizer=SGD \
 	--lr=0.01 \
 	--schedule \
@@ -27,10 +24,9 @@ python classify_mnist.py \
 	--d_dim=1000 \
 	--print_freq=20 \
 	--print_prec=2 \
-	--flips \
+	#--flips \
         #--subspace-training \
         #--non_wrapped \
         #--chunked \
         #--dense \
         #--parameter_correction
-done
